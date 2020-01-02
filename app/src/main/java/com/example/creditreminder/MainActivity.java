797fdb231aeb69d4bcd5ea74e75716640696aeb4
ExploiD2005 <exploid2005@gmail.com>
2020-01-02 18:35:16 +1100

    Start project

---
+++ app/src/main/java/com/example/creditreminder/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.creditreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
