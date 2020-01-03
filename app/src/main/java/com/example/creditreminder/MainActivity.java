d7e4d49c29895aae43a7f39121dec532820e63eb
ExploiD2005 <exploid2005@gmail.com>
2020-01-04 01:12:48 +1100

    First run

--- app/src/main/java/com/example/creditreminder/MainActivity.java
+++ app/src/main/java/com/example/creditreminder/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.creditreminder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private CreditViewModel creditViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        creditViewModel = ViewModelProviders.of(this).get(CreditViewModel.class);
+        creditViewModel.getAllCredits().observe(this, new Observer<List<Credit>>() {
+            @Override
+            public void onChanged(List<Credit> credits) {
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
