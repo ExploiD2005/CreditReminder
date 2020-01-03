2bd56da71ae41368ee39ac84b03ca834fda1a18a
ExploiD2005 <exploid2005@gmail.com>
2020-01-03 22:45:22 +1100

    Add CreditDao class

---
+++ app/src/main/java/com/example/creditreminder/CreditDao.java
@@ -0,0 +1,28 @@
+package com.example.creditreminder;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface CreditDao {
+    @Insert
+    void insert(Credit credit);
+
+    @Update
+    void update(Credit credit);
+
+    @Delete
+    void delete(Credit credit);
+
+    @Query("DELETE FROM credit_table")
+    void deleteAllCredits();
+
+    @Query("SELECT * FROM credit_table ORDER BY last_pay_date ASC")
+    LiveData<List<Credit>> getAllCredits();
+}
